ac35f04a2551a6a8cb2ba04698ee122c75f7e313
Jakob Löw <jakob@m4gnus.de>
2019-02-06 21:14:55 +0100

    messages.hpp: add some twizy can extractions manually until we generate the file automatically using genMessages.py

--- LoRaCANBridge/messages.hpp
+++ LoRaCANBridge/messages.hpp
@@ -27,6 +27,18 @@ typedef struct
 
 CANExtraction message0Extractions[] = {
 	{
+		//0x554 bytes 0-8: Cell tempertures
+		.id = 0x554,
+		.pos = 0,
+		.len = 64,
+	},
+	{
+		//0x599 bytes 0-4: Odometer
+		.id = 0x599,
+		.pos = 0,
+		.len = 32,
+	},
+	{
 		//0x155 byte 0: Charging speed in 300W
 		.id = 0x155,
 		.pos = 0,
@@ -35,17 +47,41 @@ CANExtraction message0Extractions[] = {
 	{
 		//0x155 bytes 4, 5: Battery in 0.0025%
 		.id = 0x155,
-		.pos = 4 * 8,
+		.pos = 32,
 		.len = 16,
+	},
+	{
+		//0x424 bytes 5: Battery SOH in %
+		.id = 0x424,
+		.pos = 40,
+		.len = 8,
+	},
+	{
+		//0x597 bit 0: connected to grid
+		.id = 0x597,
+		.pos = 2,
+		.len = 1,
+	},
+	{
+		//0x597 bit 9, 10, 11: offline, charging, key turned
+		.id = 0x597,
+		.pos = 9,
+		.len = 3,
+	},
+	{
+		//0x597 bit 24, 25: DC/DC status
+		.id = 0x597,
+		.pos = 24,
+		.len = 2,
 	},
 };
 
 Message messages[] = {
 	{
 		.repetition = 1,
-		.len = 19,
+		.len = 17,
 		.extractions = message0Extractions,
-		.extractionCount = sizeof(message0Extractions) / sizeof(message0Extractions),
+		.extractionCount = sizeof(message0Extractions) / sizeof(CANExtraction),
 	},
 };
 #define MESSAGE_COUNT (sizeof(messages) / sizeof(Message))
